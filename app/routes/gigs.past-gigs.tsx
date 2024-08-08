3d4a74f3f8f298c57847dffc995e2e9f7eb9d1fa
augs0 <hachigatsu14@outlook.com>
2024-08-08 16:52:24 +0100

    ✨ add past gigs with local data and loader function

--- app/routes/gigs.past-gigs.tsx
+++ app/routes/gigs.past-gigs.tsx
@@ -1,6 +1,63 @@
+import {
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+type Gigs = {
+  id: number;
+  artist: string;
+  venue: string;
+  date: string;
+};
+
+const pastGigs: Gigs[] = [
+  {
+    id: 1,
+    artist: "Maneskin",
+    venue: "Royal Arena Copenhagen",
+    date: "2023-05-04",
+  },
+  {
+    id: 2,
+    artist: "GoA",
+    venue: "Manchester Club Academy",
+    date: "2023-11-28",
+  },
+  {
+    id: 3,
+    artist: "Maneskin",
+    venue: "AO Arena Manchester",
+    date: "2023-12-17",
+  },
+];
+
+export const loader = () => {
+  return json({ pastGigs });
+};
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Past gigs" },
+    { name: "description", content: "Past gigs" },
+  ];
+};
+
 export default function PastGigs() {
-    return (
+  const data = useLoaderData<typeof loader>();
+  return (
+    <main className="mx-auto">
       <h1>Past gigs</h1>
-    )
-  }
-  
+      <ul className="flex flex-col justify-around my-4 mx-auto flex-wrap md:flex-row">
+        {data.pastGigs.map((gig) => (
+          <li className="bg-white p-4 m-3 rounded" key={gig.id}>
+            <p className="lead text-lg">{gig.artist}</p>
+            <p>{gig.venue}</p>
+            <p>{gig.date}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
